dd9d0d912d76c4142160b64fddc689dd5e017804
Jiyeon Jeon <jiyeonjeon01@naver.com>
2024-11-05 11:31:30 +0900

    [update] hr.sql 추가 공부 내용 추가

--- hr.sql
+++ hr.sql
@@ -23,6 +23,25 @@ select first_name || '의 직급은' || job_id ||'입니다' from employees;
 select first_name || '의 직급은' || job_id ||'입니다' as data from employees;
 -- 중복되지 않게 보여주기 distinct
 select distinct job_id from employees;
+-- 연봉을 3000 이상 받는 사람 정보
+select * from employees;
+desc employees;
+-- 2008년 이후에 입사한 직원 조사
+select * from employees where hire_date >= '2008/01/01';
+select * from employees where TO_CHAR(hire_date, 'YYYY/MM/DD') >= '2008/01/01';
+select * from employees where hire_date >= TO_DATE('2008/01/01', 'YYYY/MM/DD HH24:MI:SS)');
+-- AND, BETWEEN a AND b
+select * from employees where salary>=2000 and salary<=3000;
+select * from employees where salary between 2000 and 3000;
+-- OR, IN (, ) 직원 번호가 67이거나 101이거나 184인 사원
+select * from employees where employee_ID=67 or employee_ID=101 or employee_ID=184;
+select * from employees where employee_ID in (67, 101, 184);
+-- null 연산, 비교, 할당 안된다
+select 100 + null from dual;
+select * from employees where commission_pct is not null;
+select * from employees where commission_pct is null;
+select employee_id, first_name, commission_pct from employees order by employee_id desc;
+
 
 
 
