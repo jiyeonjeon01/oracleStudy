6166bab7bdcd483b667283c0648341537b5af9ad
Jiyeon Jeon <jiyeonjeon01@naver.com>
2024-12-18 07:34:19 +0900

    [update] site를 만들기 위한 sql 다시 재정의 찐막.

--- webuser/메모장table.sql
+++ webuser/메모장table.sql
@@ -156,6 +156,19 @@ FROM
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------
 --회원가입 
 -- 학생관리 테이블
@@ -171,29 +184,172 @@ CREATE TABLE student (
     address1 VARCHAR2(120) NOT NULL,
     address2 VARCHAR2(50) NOT NULL
 );
-
 ALTER TABLE student ADD CONSTRAINT student_id_pk PRIMARY KEY ( id );
 
-CREATE TABLE zipcode (
-    seq     NUMBER(10) NOT NULL,
-    zipcode VARCHAR2(50),
-    sido    VARCHAR2(50),
-    gugun   VARCHAR2(50),
-    dong    VARCHAR2(50),
-    bunji   VARCHAR2(50)
+-- 어드민 테이블
+CREATE TABLE ADMIN (
+    id       VARCHAR2(12) NOT NULL,
+    pass     VARCHAR2(12) NOT NULL,
+    name     VARCHAR2(10) NOT NULL,
+    phone1   VARCHAR2(3) NOT NULL,
+    phone2   VARCHAR2(4) NOT NULL,
+    phone3   VARCHAR2(4) NOT NULL,
+    email    VARCHAR2(30) NOT NULL,
+    zipcode  VARCHAR2(7) NOT NULL,
+    address1 VARCHAR2(120) NOT NULL,
+    address2 VARCHAR2(50) NOT NULL
 );
+ALTER TABLE ADMIN ADD CONSTRAINT ADMIN_id_pk PRIMARY KEY ( id );
 
+-- 주소
+CREATE TABLE zipcode (
+    seq         NUMBER(10) PRIMARY KEY,
+    zipcode     VARCHAR2(10) NOT NULL, -- 우편번호
+    sido        VARCHAR2(30),         -- 시/도
+    gugun       VARCHAR2(30),         -- 구/군
+    dong        VARCHAR2(50),         -- 동/읍/면
+    bunji       VARCHAR2(100)         -- 번지
+);
 ALTER TABLE zipcode ADD CONSTRAINT zipcode_seq_pk PRIMARY KEY ( seq );
 
-ALTER TABLE zipcode MODIFY BUNJI VARCHAR2(100);
-DESCRIBE ZIPCODE;
 
 SELECT * FROM ZIPCODE;
-
+SELECT * FROM ADMIN;
 select * from student;
+commit;
+SELECT * FROM zipcode WHERE dong LIKE '%신촌동%';
 
-select * from zipcode where dong like '박촌%';
 
-SELECT PASS FROM STUDENT WHERE ID = 'aaaa';
+------------------------------------------------------------------------------
+-- 답변에만 쓰임 
+--REF       NUMBER(5,0) DEFAULT 0, 
+--    STEP        NUMBER(3,0) DEFAULT 0 , 
+--    DEPTH     NUMBER(3,0) DEFAULT 0, 
+    
+    
+--  공지 게시판
+CREATE TABLE NOTIBOARD (
+    "NUM"         NUMBER(7,0) NOT NULL , 
+    WRITER      VARCHAR2(20) NOT NULL, 
+    EMAIL       VARCHAR2(30) NOT NULL, 
+    SUBJECT     VARCHAR2(50) NOT NULL,  
+    READCOUNT   NUMBER(5,0) DEFAULT 0, 
+    REGDATE     TIMESTAMP (6) DEFAULT SYSDATE, 
+    "CONTENT"   VARCHAR2(4000) NOT NULL , 
+    IP          VARCHAR2(45) NOT NULL ,
+    ORIGINFILE VARCHAR2(255), -- 원본 파일명
+    SYSFILE VARCHAR2(255)    -- 시스템에 저장된 파일명
+   );
+ ALTER TABLE NOTIBOARD ADD CONSTRAINT NOTIBOARD_NUM_PK PRIMARY KEY (NUM);
+ CREATE SEQUENCE NOTIBOARD_SEQ
+ START WITH 1
+ INCREMENT BY 1
+ NOMAXVALUE
+ NOCACHE
+ NOCYCLE;
+ select * from NOTIboard;
+ commit;
+ 
+ 
+ 
+ --  자유 게시판
+CREATE TABLE NORMALBOARD (
+    "NUM"         NUMBER(7,0) NOT NULL , 
+    WRITER      VARCHAR2(20) NOT NULL, 
+    EMAIL       VARCHAR2(30) NOT NULL, 
+    SUBJECT     VARCHAR2(50) NOT NULL, 
+    PASS        VARCHAR2(10) NOT NULL , 
+    READCOUNT   NUMBER(5,0) DEFAULT 0, 
+    "REF"       NUMBER(5,0) DEFAULT 0, 
+    STEP        NUMBER(3,0) DEFAULT 0 , 
+    "DEPTH"     NUMBER(3,0) DEFAULT 0, 
+    REGDATE     TIMESTAMP (6) DEFAULT SYSDATE, 
+    "CONTENT"   VARCHAR2(4000) NOT NULL , 
+    IP          VARCHAR2(45) NOT NULL ,
+    ORIGINFILE VARCHAR2(255), -- 원본 파일명
+    SYSFILE VARCHAR2(255)    -- 시스템에 저장된 파일명
+   );
+ ALTER TABLE NORMALBOARD ADD CONSTRAINT NORMALBOARD_NUM_PK PRIMARY KEY (NUM);
+ CREATE SEQUENCE NORMALBOARD_SEQ
+ START WITH 1
+ INCREMENT BY 1
+ NOMAXVALUE
+ NOCACHE
+ NOCYCLE;
+ select * from NORMALboard;
+ commit;
 
-SELECT * FROM STUDENT WHERE ID = 'aaaa';
+
+
+
+-- QNA게시판
+CREATE TABLE QNABOARD (
+    "NUM"         NUMBER(7,0) NOT NULL , 
+    WRITER      VARCHAR2(20) NOT NULL, 
+    EMAIL       VARCHAR2(30) NOT NULL, 
+    SUBJECT     VARCHAR2(50) NOT NULL, 
+    PASS        VARCHAR2(10) NOT NULL , 
+    READCOUNT   NUMBER(5,0) DEFAULT 0, 
+    "REF"       NUMBER(5,0) DEFAULT 0, 
+    STEP        NUMBER(3,0) DEFAULT 0 , 
+    "DEPTH"     NUMBER(3,0) DEFAULT 0, 
+    REGDATE     TIMESTAMP (6) DEFAULT SYSDATE, 
+    "CONTENT"   VARCHAR2(4000) NOT NULL , 
+    IP          VARCHAR2(45) NOT NULL ,
+    ORIGINFILE VARCHAR2(255), -- 원본 파일명
+    SYSFILE VARCHAR2(255)    -- 시스템에 저장된 파일명
+   );
+ ALTER TABLE QNABOARD ADD CONSTRAINT QNABOARD_NUM_PK PRIMARY KEY (NUM);
+ CREATE SEQUENCE QNABOARD_SEQ
+ START WITH 1
+ INCREMENT BY 1
+ NOMAXVALUE
+ NOCACHE
+ NOCYCLE;
+ select * from QNAboard;
+ commit;
+ 
+ 
+ 
+ -- 쇼핑 게시판
+CREATE TABLE SHOPPINGBOARD (
+    "NUM"         NUMBER(7,0) NOT NULL , 
+    WRITER      VARCHAR2(20) NOT NULL, 
+    EMAIL       VARCHAR2(30) NOT NULL, 
+    SUBJECT     VARCHAR2(50) NOT NULL,  
+    READCOUNT   NUMBER(5,0) DEFAULT 0, 
+    REGDATE     TIMESTAMP (6) DEFAULT SYSDATE, 
+    "CONTENT"   VARCHAR2(4000) NOT NULL , 
+    IP          VARCHAR2(45) NOT NULL,
+    ORIGINFILE VARCHAR2(255), -- 원본 파일명
+    SYSFILE VARCHAR2(255)    -- 시스템에 저장된 파일명
+   );
+ ALTER TABLE SHOPPINGBOARD ADD CONSTRAINT SHOPPINGBOARD_NUM_PK PRIMARY KEY (NUM);
+ CREATE SEQUENCE SHOPPINGBOARD_SEQ
+ START WITH 1
+ INCREMENT BY 1
+ NOMAXVALUE
+ NOCACHE
+ NOCYCLE;
+ select * from SHOPPINGboard;
+ commit;
+ 
+ -- 댓글 
+CREATE TABLE NORMALREPLY (
+    NUM    NUMBER(7,0) NOT NULL,    
+    NORMALNUM    NUMBER(7,0) NOT NULL, --FK
+    WRITER      VARCHAR2(20) NOT NULL,  
+    CONTENT     VARCHAR2(1000) NOT NULL,   
+    REGDATE     TIMESTAMP DEFAULT SYSDATE,   
+    IP          VARCHAR2(45) NOT NULL     
+);
+ALTER TABLE NORMALREPLY ADD CONSTRAINT NORMALREPLY_PK PRIMARY KEY (NUM);
+ALTER TABLE NORMALREPLY ADD CONSTRAINT NORMALREPLY_NORMALNUM_FK FOREIGN KEY (NORMALNUM) 
+    REFERENCES NORMALBOARD (NUM) ON DELETE CASCADE;
+CREATE SEQUENCE NORMALREPLY_SEQ
+    START WITH 1
+    INCREMENT BY 1
+    NOMAXVALUE
+    NOCACHE
+    NOCYCLE;
+commit;
